bc7fb6b149cd0978c9725624b624109a8431302b
Naveen Kharwar <naveenkharwar@hotmail.com>
2020-07-11 12:34:52 +0530

    Base Font Change Raleway to Nuntu

--- functions.php
+++ functions.php
@@ -147,7 +147,9 @@ function awp_scripts() {
 	wp_style_add_data( 'awp-style', 'rtl', 'replace' );
 
     wp_enqueue_script( 'awp-navigation', get_template_directory_uri() . '/public/js/navigation.js', array(), _S_VERSION, true );
-    wp_enqueue_style('awp-font', '//fonts.googleapis.com/css2?family=Catamaran:wght@100;200;300;400;500;600;700;800;900&family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    wp_enqueue_style('awp-playfair-font', '//fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    wp_enqueue_style('awp-catamaran-font', '//fonts.googleapis.com/css2?family=Catamaran:wght@100;200;300;400;500;600;700;800;900&display=swap');
+    wp_enqueue_style('awp-nunito-font', '//fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap');
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
