da1da8472c2dfeaea5fc8c970f271568979a600c
Naveen Kharwar <naveenkharwar@hotmail.com>
2020-07-22 21:43:25 +0530

    npm fund fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,3 +10,5 @@ mix.js('resources/js/customizer.js', 'public/js')
     .js('resources/js/navigation.js', 'public/js')
     .sass('resources/sass/style.scss','')
     .copy('node_modules/bootstrap/dist/js/bootstrap.min.js','public/bootstrap/js');
+
+mix.disableNotifications();
