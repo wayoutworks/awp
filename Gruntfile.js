10313d56cc9755f39ffce3ab31fc770891ebe3df
Naveen Kharwar <naveenkharwar@hotmail.com>
2020-07-25 14:07:03 +0530

    Change: Change Dev Environment from Laravel-Mix To Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,64 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      sass: {
-        dist: {
-          options: {
-            style: 'expanded',
-          },
-          files: {
-            'style.css' : 'resources/sass/style.scss'
-          }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON("package.json"),
+    sass: {
+      dist: {
+        options: {
+          style: "expanded",
+        },
+        files: {
+          "style.css": "resources/sass/style.scss",
         },
       },
-      cssmin: {
+    },
+    cssmin: {
+      options: {
+        mergeIntoShorthands: false,
+        roundingPrecision: -1,
+        report: "gzip",
+      },
+      target: {
+        src: ["style.css"],
+        dest: "style.min.css",
+      },
+    },
+    copy: {
+      build: {
+        src: ["**", "!node_modules/**", "!vendor/**", "!Gruntfile.js", "!package.json", "!package-lock.json", "!resources/**", "!composer.json", "!composer.lock", "!mix.manifest.json", "!phpcs.xml.dist", "!README.md", "!style.css.map", '!webpack.mix.js'],
+        dest: "build/",
+      },
+      clean: {
+        build: {
+          src: ['build/']
+        }
+      },
+    },
+    compress: {
+      build: {
         options: {
-            mergeIntoShorthands: false,
-            roundingPrecision: -1,
-            report: 'gzip'
-          },
-        target : {
-            src : ["style.css",],
-            dest : "style.min.css"
-        }
+          archive: 'awp.zip',
+        },
+        files: [{
+          expand: true,
+          cwd: 'build/',
+          src: ['**/*'],
+          dist: '/'
+        }]
+      }
+    },
+    watch: {
+      css: {
+        files: "**/*.scss",
+        tasks: ["sass", "cssmin"],
       },
-      watch: {
-        css: {
-          files: '**/*.scss',
-          tasks: ['sass','cssmin']
-        }
-      },
-    });
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    // Watch Task
-    grunt.registerTask('default',['watch']);
+    },
+  });
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-compress");
+
+  // Watch Task
+  grunt.registerTask("default", ["watch"]);
 };
